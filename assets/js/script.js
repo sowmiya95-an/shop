1c3ce4d01f538708649e24651a2da894a85e9f57
sowmiya95-an <68177152+sowmiya95-an@users.noreply.github.com>
2024-08-20 13:34:12 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+
+let items = document.querySelectorAll('.prodcarousel .carousel-item')
+
+		items.forEach((el) => {
+			const minPerSlide = 3
+			let next = el.nextElementSibling
+			for (var i=1; i<minPerSlide; i++) {
+				if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+$(document).ready(function() {
+   
+    
+    $("#news-slider2").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[600,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay:true
+    });
+    
+  
+   
+});
+
